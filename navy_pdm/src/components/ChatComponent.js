8fd1e3e203fd3aff461065cc20a4d3b50995a0bf
Will Yuponce <will.yuponce@databricks.com>
2025-07-23 10:46:42 -0700

    Call to genie

--- navy_pdm/src/components/ChatComponent.js
+++ navy_pdm/src/components/ChatComponent.js
@@ -5,15 +5,54 @@ import SendIcon from "@mui/icons-material/Send";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import axios from "axios";
 function ChatComponent(props) {
+  const [chatMessages, setChatMessages] = useState([]);
+  const [messageNumber, setMessageNumber] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
-  function openDialog() {
-    setIsOpen(true);
+  const [chatField, setChatField] = useState("");
+  function toggleDialog() {
+    setIsOpen(!isOpen);
+  }
+  function formInput(event) {
+    setChatField(event.target.value);
   }
   function closeDialog() {
     setIsOpen(false);
+  }
+
+  function sendMessage(event) {
+    appendMessage({ text: chatField, isUser: true });
+    axios
+      .post(
+        "https://e2-demo-field-eng.cloud.databricks.com/api/2.0/genie/spaces/01f055d6ca9f1bf59693113af3d9280f/start-conversation",
+        {
+          content: chatField,
+        },
+        {
+          //Adding token to the request
+          headers: {
+            Authorization: "Bearer dapi8a974c6446ea7534a358355ee4e8b56a", //gitleaks:allow
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+            "Access-Control-Allow-Headers": "Content-Type, x-requested-with",
+          },
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+      });
+    setChatField("");
+  }
+  function appendMessage(message) {
+    setChatMessages([...chatMessages, message]);
   }
   return (
     <div className="ChatComponent">
@@ -28,13 +67,56 @@ function ChatComponent(props) {
             right: "20px",
           }}
         >
-          <div>
+          <div style={{ display: "flex", alignItems: "center" }}>
             <CloseIcon
               onClick={closeDialog}
               style={{ display: "flex", color: "gray", padding: "10px" }}
             />
-            <h1 style={{textAlign: "center"}}>Hello</h1>
+            <p1
+              style={{
+                position: "absolute",
+                left: "110px",
+                textAlign: "center",
+              }}
+            >
+              Databricks Predictive Maintenance Genie
+            </p1>
           </div>
+          <Divider variant="middle" />
+          <Stack direction="column" style={{ margin: "10px" }} spacing={2}>
+            {chatMessages.map((message) => {
+              if (message.isUser) {
+                return (
+                  <Chip
+                    sx={{
+                      height: "auto",
+                      "& .MuiChip-label": {
+                        display: "block",
+                        whiteSpace: "normal",
+                      },
+                    }}
+                    label={message.text}
+                    style={{ marginLeft: "100px" }}
+                  />
+                );
+              } else {
+                return (
+                  <Chip
+                    sx={{
+                      height: "auto",
+                      "& .MuiChip-label": {
+                        display: "block",
+                        whiteSpace: "normal",
+                      },
+                    }}
+                    label={message.text}
+                    style={{ marginRight: "100px" }}
+                    variant="outlined"
+                  />
+                );
+              }
+            })}
+          </Stack>
           <div
             style={{
               display: "flex",
@@ -49,12 +131,19 @@ function ChatComponent(props) {
               gap: "10px",
             }}
           >
-            <TextField
-              style={{ width: "100%" }}
-              id="outlined-basic"
-              variant="outlined"
-            />
-            <Button style={{ gap: "10px", padding: "20px" }}>
+            <FormControl style={{ width: "100%" }}>
+              <TextField
+                onChange={formInput}
+                id="outlined-basic"
+                variant="outlined"
+                value={chatField}
+              />
+            </FormControl>
+
+            <Button
+              onClick={sendMessage}
+              style={{ gap: "10px", padding: "20px" }}
+            >
               Send
               <SendIcon />
             </Button>
@@ -69,7 +158,7 @@ function ChatComponent(props) {
         }}
         color="primary"
         aria-label="add"
-        onClick={openDialog}
+        onClick={toggleDialog}
       >
         <ChatIcon />
       </Fab>
