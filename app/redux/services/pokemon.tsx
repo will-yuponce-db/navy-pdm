f0abed6fa16b0d38f6a10104cab87dd62b75fc4c
Will Yuponce <will.yuponce@databricks.com>
2025-09-04 08:54:46 -0400

    Formatting

--- app/redux/services/pokemon.tsx
+++ app/redux/services/pokemon.tsx
@@ -1,25 +1,32 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
-  reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://e2-demo-field-eng.cloud.databricks.com/api/2.0/sql/statements', headers:{Authorization: "Bearer dapiec2b97816dbe739bb8990ce144fe79a4"} }), //gitleaks:allow
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      "https://e2-demo-field-eng.cloud.databricks.com/api/2.0/sql/statements/",
+    headers: {
+      Authorization: "Bearer dapiec2b97816dbe739bb8990ce144fe79a4", //gitleaks:allow
+      "Content-Type": "application/json",
+    },
+  }), //gitleaks:allow
   endpoints: (build) => ({
     getPokemonByName: build.query({
       query: (sql) => ({
-        url: '',
-        method: 'POST',
+        url: "",
+        method: "POST",
         body: {
-  "warehouse_id": "8baced1ff014912d",
-  "catalog": "odni",
-  "schema": "tagging",
-  "statement": "SELECT * from joined_data LIMIT 2000"
-},
+          warehouse_id: "8baced1ff014912d",
+          catalog: "odni",
+          schema: "tagging",
+          statement: "SELECT * from joined_data LIMIT 2000",
+        },
       }),
     }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi
+export const { useGetPokemonByNameQuery } = pokemonApi;
